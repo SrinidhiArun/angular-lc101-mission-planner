1fa2f434ed49ecc693aaf6a1e21d85c8f4d02f86
Arun Balasubramanian <srinidhi>
2020-11-04 20:29:09 -0600

    Code for studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,10 +12,38 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
-
+  memberBeingEdited: object = null;
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(memberName: string, isFirst: boolean) {
+    let found = false;
+    for(let i = 0; i < this.crew.length; i++) {
+    if ((this.crew[i])[name] == memberName) {
+        found = true;
+        break;
+    }
+  }
+    if(!found){
+    this.crew.push({name: memberName, firstMission: isFirst});
+    }
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+}
+  
 
 }
--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipments: String[] =["Habitat dome","Drones","Food containers","Oxygen tanks"];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  add(name: String){
+    this.equipments.push(name);
+  }
+  remove(name: String){
+    let index = this.equipments.indexOf(name);
+    this.equipments.splice(index,1)
   }
 
 }
